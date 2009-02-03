62e77e0a11c19f9aa84f217a4f10c614f3956811
Christian Engwer <christi@dune-project.org>
2009-02-03 10:10:19 +0000

    skeleton for the buildsystem
    [[Imported from SVN: r7]]

---
+++ dune/marchingcubes/marchingcubes.hh
@@ -0,0 +1,12 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef MARCHINGCUBES_HH
+#define MARCHINGCUBES_HH
+
+#include <vector>
+#include <iostream>
+#include <dune/common/geometrytype.hh>
+
+namespace Dune {} // end namespace Dune
+
+#endif // MARCHINGCUBES_HH
---
+++ m4/dune-mc.m4
@@ -0,0 +1,4 @@
+# Additional checks needed to build the module
+AC_DEFUN([DUNE_MC_CHECKS])
+# Additional checks needed to find the module
+AC_DEFUN([DUNE_MC_CHECK_MODULE])
