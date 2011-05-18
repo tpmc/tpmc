076f1a3d3082305994021c076c1829995f1ce62e
Oliver Sander <sander@dune-project.org>
2011-05-18 16:08:38 +0000

    A rudimentary test for the Dune wrapper for marching cubes
    [[Imported from SVN: r110]]

---
+++ dune/marchingcubes/test/testmarchingcubesrefinement.cc
@@ -0,0 +1,32 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <iostream>
+
+#include <dune/marchingcubes/marchingcubesrefinement.hh>
+
+using namespace Dune;
+
+// Very primitive testing of the Dune wrapper for the marching cubes algorithm
+int main(int argc, char* argv[])
+{
+  GeometryType quadrilateral;
+  quadrilateral.makeQuadrilateral();
+
+  std::vector<double> values(4);
+  values[0] = values[1] = -1;
+  values[2] = values[3] =  1;
+
+  MarchingCubesRefinement<double,2> refinement(quadrilateral, values);
+
+  std::cout << "Elements:" << std::endl;
+  MarchingCubesRefinement<double,2>::const_iterator it = refinement.begin();
+
+  for (; it!=refinement.end(); ++it) {
+
+    std::cout << "element has " << it->corners() << " corners" << std::endl;
+    for (int i=0; i<it->corners(); i++)
+      std::cout << it->corner(i) << std::endl;
+
+  }
+
+}
