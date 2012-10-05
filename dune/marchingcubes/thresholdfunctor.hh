d036a5dabd855bfd32a9a31acc704a2778c2c0ed
Christian Engwer <christi@dune-project.org>
2012-10-05 10:02:03 +0000

    [threshold] allow to set the threshold dynamically, add a special class for the static threshold value
    [[Imported from SVN: r188]]

--- dune/marchingcubes/thresholdfunctor.hh
+++ dune/marchingcubes/thresholdfunctor.hh
@@ -11,9 +11,22 @@
 namespace Dune {
   namespace MarchingCubes {
 
+    /**
+     * \brief basic threshold functor
+     *
+     * - $u < t$   : inside
+     * - $u \ge t$ : outside
+     *
+     * the threshold $t$ is a runtime parameter of the constructor
+     *
+     * \param vtype data type of the threshold value, e.g. the data type of the marching cubes class
+     */
     template <class vtype>
     class ThresholdFunctor {
     public:
+
+      //! setup the ThresholdFunctor with a given threshold value
+      ThresholdFunctor(value v = 0.0) : threshold(v) {}
 
       //! Function to test whether a point is inside the isosurface.
       bool isInside(vtype testValue) const
@@ -51,7 +64,70 @@ namespace Dune {
 
     private:
       //! Defines the isosurface.
-      constexpr static vtype threshold = 0.0;
+      vtype threshold;
+    };
+
+
+    /**
+     * \brief basic threshold functor
+     *
+     * - $u < t$   : inside
+     * - $u \ge t$ : outside
+     *
+     * the threshold $t$ and the data type are obtained statically from the ValueTraits
+     *
+     * ValueTraits are an implementation of the following type:
+     * \code
+     * struct ValueTraits {
+     *   typedef double ValueType;
+     *   static const double value;
+     * };
+     *  static const double ValueTraits::value = 17.3;
+     * \endcode
+     */
+    template <typename ValueTraits>
+    class StaticThresholdFunctor {
+    public:
+
+      typedef ValueTraits::ValueType vtype;
+
+      //! Function to test whether a point is inside the isosurface.
+      bool isInside(vtype testValue) const
+      {
+        return (testValue < threshold);
+      }
+
+      //! distance to iso surface
+      vtype getDistance(vtype value) const
+      {
+        return value - threshold;
+      }
+
+      //! minimum length for an edge below which it will be considered
+      //! to be degenerated
+      vtype degenerationDistance() const
+      {
+        return 1e-8;
+      }
+
+      //! distance to iso surface
+      template<typename point>
+      vtype interpolationFactor
+        (const point &, const point& , const vtype va, const vtype vb) const
+      {
+        const vtype dist = getDistance(vb)-getDistance(va);
+        return getDistance(va) / dist;
+      }
+
+      //! isInside for distance value
+      bool isLower(vtype distance) const
+      {
+        return distance < 0;
+      }
+
+    private:
+      //! Defines the isosurface.
+      constexpr static vtype threshold = ValueTraits::value;
     };
 
 
@@ -63,7 +139,7 @@ namespace Dune {
 
       //! constructor
       NonDegeneratingThresholdFunctor(const Geometry & _embedding, const vtype _minLength)
-        : embedding(_embedding), minLength(_minLength)
+        : ThresholdFunctor<vtype>(0), embedding(_embedding), minLength(_minLength)
       {}
 
       //! minimum length for an edge below which it will be considered
