b5472afa2f670c7776d3bfd8688dc2f7796a0a3c
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2012-10-22 15:19:08 +0000

    fixed problem with isdegenerated: IsDegeneratedBase<ctype> is a dependent base class and therefore IsDegeneratedBase<ctype>::eq is not considered when looking up the unqualified eq in IsDegenerated<ctype>
    [[Imported from SVN: r199]]

--- dune/marchingcubes/isdegenerated.hh
+++ dune/marchingcubes/isdegenerated.hh
@@ -48,16 +48,18 @@ namespace Dune {
 
   template <class ctype>
   struct IsDegenerated<ctype, 1> : IsDegeneratedBase<ctype> {
+    typedef IsDegeneratedBase<ctype> BaseT;
     enum { dim = 1 };
     template<int d>
     static bool check (std::vector< FieldVector<ctype,d> > & c) {
-      return eq(c[0],c[1]);
+      return BaseT::eq(c[0],c[1]);
     }
   };
 
   /** \brief Specialization for 2d elements */
   template <class ctype>
   struct IsDegenerated<ctype, 2> : IsDegeneratedBase<ctype> {
+    typedef IsDegeneratedBase<ctype> BaseT;
     enum { dim = 2 };
 
     /** \brief Delete the entry with number 'pos' from the vector 'c'
@@ -76,16 +78,16 @@ namespace Dune {
       switch (c.size())
       {
       case 3 :
-        return (eq(c[0],c[1]) || eq(c[0],c[2]) || eq(c[1],c[2]) );
+        return (BaseT::eq(c[0],c[1]) || BaseT::eq(c[0],c[2]) || BaseT::eq(c[1],c[2]) );
       case 4 :
-        if (eq(c[0],c[1])) { erase(1,c); return check(c); }
-        if (eq(c[0],c[2])) { erase(2,c); return check(c); }
-        if (eq(c[0],c[3])) { return true; }
-        if (eq(c[1],c[2])) { return true; }
-        if (eq(c[1],c[3])) { erase(3,c); return check(c); }
-        if (eq(c[2],c[3])) { erase(3,c); return check(c); }
-        return ( (eq(c[0],c[1]) && eq(c[2],c[3])) ||
-                 (eq(c[0],c[2]) && eq(c[1],c[3])) );
+        if (BaseT::eq(c[0],c[1])) { erase(1,c); return check(c); }
+        if (BaseT::eq(c[0],c[2])) { erase(2,c); return check(c); }
+        if (BaseT::eq(c[0],c[3])) { return true; }
+        if (BaseT::eq(c[1],c[2])) { return true; }
+        if (BaseT::eq(c[1],c[3])) { erase(3,c); return check(c); }
+        if (BaseT::eq(c[2],c[3])) { erase(3,c); return check(c); }
+        return ( (BaseT::eq(c[0],c[1]) && BaseT::eq(c[2],c[3])) ||
+                 (BaseT::eq(c[0],c[2]) && BaseT::eq(c[1],c[3])) );
       default :
         DUNE_THROW(Dune::Exception, "Impossible Geometry. " <<
                    "In 2D there is no known geometry with " <<
@@ -97,6 +99,7 @@ namespace Dune {
   /** \brief Specialization for 3d elements */
   template <class ctype>
   struct IsDegenerated<ctype, 3> : IsDegeneratedBase<ctype> {
+    typedef IsDegeneratedBase<ctype> BaseT;
     enum { dim = 3 };
 
     /** \brief Return true if the element with the corners given by 'c' is degenerate */
@@ -105,37 +108,40 @@ namespace Dune {
       switch (c.size())
       {
       case 4 :
-        return ( eq(c[0],c[1]) ||
-                 eq(c[0],c[2]) ||
-                 eq(c[0],c[3]) ||
-                 eq(c[1],c[2]) ||
-                 eq(c[1],c[3]) ||
-                 eq(c[2],c[3]));
+        return ( BaseT::eq(c[0],c[1]) ||
+                 BaseT::eq(c[0],c[2]) ||
+                 BaseT::eq(c[0],c[3]) ||
+                 BaseT::eq(c[1],c[2]) ||
+                 BaseT::eq(c[1],c[3]) ||
+                 BaseT::eq(c[2],c[3]));
       case 5 :
       {
         bool deg=false;
-        deg |= eq(c[3],c[0]);
-        deg |= eq(c[2],c[1]);
+        deg |= BaseT::eq(c[3],c[0]);
+        deg |= BaseT::eq(c[2],c[1]);
         int mat(0);
-        mat += int(eq(c[0],c[1]));
-        mat += int(eq(c[0],c[2]));
-        mat += int(eq(c[0],c[4]));
-        mat += int(eq(c[1],c[3]));
-        mat += int(eq(c[1],c[4]));
-        mat += int(eq(c[2],c[3]));
-        mat += int(eq(c[2],c[4]));
-        mat += int(eq(c[3],c[4]));
+        mat += int(BaseT::eq(c[0],c[1]));
+        mat += int(BaseT::eq(c[0],c[2]));
+        mat += int(BaseT::eq(c[0],c[4]));
+        mat += int(BaseT::eq(c[1],c[3]));
+        mat += int(BaseT::eq(c[1],c[4]));
+        mat += int(BaseT::eq(c[2],c[3]));
+        mat += int(BaseT::eq(c[2],c[4]));
+        mat += int(BaseT::eq(c[3],c[4]));
         return deg || mat > 1;
       }
       case 6 :
-        return ( (eq(c[0],c[3]) && eq(c[1],c[4]) && eq(c[2],c[5])) ||
-                 (eq(c[0],c[1]) && eq(c[3],c[4])) ||
-                 (eq(c[0],c[2]) && eq(c[3],c[5])) ||
-                 (eq(c[1],c[2]) && eq(c[4],c[5])) );
+        return ( (BaseT::eq(c[0],c[3]) && BaseT::eq(c[1],c[4]) && BaseT::eq(c[2],c[5])) ||
+                 (BaseT::eq(c[0],c[1]) && BaseT::eq(c[3],c[4])) ||
+                 (BaseT::eq(c[0],c[2]) && BaseT::eq(c[3],c[5])) ||
+                 (BaseT::eq(c[1],c[2]) && BaseT::eq(c[4],c[5])) );
       case 8 :
-        return ( (eq(c[0],c[1]) && eq(c[2],c[3]) && eq(c[4],c[5]) && eq(c[6],c[7])) ||
-                 (eq(c[0],c[2]) && eq(c[1],c[3]) && eq(c[4],c[6]) && eq(c[5],c[7])) ||
-                 (eq(c[0],c[4]) && eq(c[1],c[5]) && eq(c[2],c[6]) && eq(c[3],c[7])) );
+        return ( (BaseT::eq(c[0],c[1]) && BaseT::eq(c[2],c[3])
+                  && BaseT::eq(c[4],c[5]) && BaseT::eq(c[6],c[7])) ||
+                 (BaseT::eq(c[0],c[2]) && BaseT::eq(c[1],c[3])
+                  && BaseT::eq(c[4],c[6]) && BaseT::eq(c[5],c[7])) ||
+                 (BaseT::eq(c[0],c[4]) && BaseT::eq(c[1],c[5])
+                  && BaseT::eq(c[2],c[6]) && BaseT::eq(c[3],c[7])) );
       default :
         DUNE_THROW(Dune::Exception, "Impossible Geometry. " <<
                    "In 3D there is no known geometry with " <<
