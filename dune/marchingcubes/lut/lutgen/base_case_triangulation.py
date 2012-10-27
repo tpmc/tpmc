98f66e2bca52977de911ea2cf1eb4e31355848f5
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2012-10-27 15:28:45 +0000

    finished symmetric case 7.2
    [[Imported from SVN: r212]]

--- dune/marchingcubes/lut/lutgen/base_case_triangulation.py
+++ dune/marchingcubes/lut/lutgen/base_case_triangulation.py
@@ -962,11 +962,11 @@ BCTcube3dsym.base_cases[9].tests = binaryheap((TEST_FACE_4,
 # 0,1,1,0,1,0,0,0 -> 00010110 # MC33 Case 7.2 (face 0, 2 inside; face 4 outside)
 BCTcube3dsym.base_cases[6].mc33.append(Triangulation())
 BCTcube3dsym.base_cases[6].mc33[-1].name = "MC33 Case 7.2 face 0 and 2 inside"
-BCTcube3dsym.base_cases[6].mc33[-1].faces = []
-BCTcube3dsym.base_cases[6].mc33[-1].exterior = []
-BCTcube3dsym.base_cases[6].mc33[-1].exterior_groups = [0, 1, 1, 1]
-BCTcube3dsym.base_cases[6].mc33[-1].interior = []
-BCTcube3dsym.base_cases[6].mc33[-1].interior_groups = [2, 2, 2, 2, 2, 2, 2, 2]
+BCTcube3dsym.base_cases[6].mc33[-1].faces = [[(0, 4), (4, 6), (4, 5)], [(0, 2), (2, 6), (1, 5)], [(0, 2), (0, 1), (1, 5)], [(1, 3), (2, 3), (1, 5)], [(1, 5), (2, 6), (2, 3)]]
+BCTcube3dsym.base_cases[6].mc33[-1].exterior = [[(0, 4), (4, 5), (4, 6), 4], [(0, 2), (2, 3), (2, 6), 2], [(0, 1), (1, 3), (1, 5), 1], [(0, 1), (1, 3), Face4, (1, 5)], [(0, 2), (2, 3), (2, 6), Face4], [(1, 3), (2, 3), Face4, (1, 5)], [(0, 1), (0, 2), Face4, (1, 5)], [(2, 3), (2, 6), (1, 5), Face4], [(0, 2), (2, 6), (1, 5), Face4]]
+BCTcube3dsym.base_cases[6].mc33[-1].exterior_groups = [0,1,1,1,1,1,1,1,1]
+BCTcube3dsym.base_cases[6].mc33[-1].interior = [[(1, 3), (1, 5), (2, 3), 7], [(2, 3), (1, 5), (2, 6), 7], [(4, 5), (4, 6), (1, 5), 7], [(1, 5), (2, 6), 7, (4, 6)], [(1, 5), (0, 2), (2, 6), Face0], [(1, 5), (2, 6), (4, 6), Face0], [(4, 6), (1, 5), Face2, (4, 5)], [(1, 5), Face0, Face2, (4, 6)], [(0, 4), (4, 6), Face2, (4, 5)], [Face0, Face2, (0, 4), (4, 6)], [(0, 1), (1, 5), Face2, (0, 2)], [(0, 2), Face2, (1, 5), Face0], [(0, 1), (0, 2), Face2, (0, 4)], [Face0, Face2, (0, 2), (0, 4)], [(0, 1), (0, 2), (0, 4), 0], [(2, 6), (4, 6), 6, 7], [(1, 5), (4, 5), 5, 7], [(1, 3), (2, 3), 3, 7]]
+BCTcube3dsym.base_cases[6].mc33[-1].interior_groups = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 # 0,1,1,0,1,0,0,0 -> 00010110 # MC33 Case 7.2 (face 2, 4 inside; face 0 outside)
 BCTcube3dsym.base_cases[6].mc33.append(Triangulation())
 BCTcube3dsym.base_cases[6].mc33[-1].name = "MC33 Case 7.2 face 2 and 4 inside"
