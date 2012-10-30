a21fc2478b5802c29eddb0d0dbba41a29c5d21c8
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2012-10-30 09:50:23 +0000

    fixed symmetric case 4.1 and 4.2
    [[Imported from SVN: r217]]

--- dune/marchingcubes/lut/lutgen/base_case_triangulation.py
+++ dune/marchingcubes/lut/lutgen/base_case_triangulation.py
@@ -876,11 +876,11 @@ BCTcube3dsym.base_cases[7].interior = [[(4, 6), (2, 6), 6, 7], [(1, 5), (4, 5), 
 BCTcube3dsym.base_cases[7].interior_groups = [1,1,1,1,1,1,1]
 # 0,0,0,1,1,0,0,0 -> 00011000 # Basic Case 4
 BCTcube3dsym.base_cases[8].name = "MC33 Case 4.1"
-BCTcube3dsym.base_cases[8].faces = []
-BCTcube3dsym.base_cases[8].exterior = []
+BCTcube3dsym.base_cases[8].faces = [[(0, 4), (4, 5), (4, 6)], [(1, 3), (2, 3), (3, 7)]]
+BCTcube3dsym.base_cases[8].exterior = [[4, (0, 4), (4, 5), (4, 6)], [(1, 3), (2, 3), (3, 7), 3]]
 BCTcube3dsym.base_cases[8].exterior_groups = [1, 0]
-BCTcube3dsym.base_cases[8].interior = []
-BCTcube3dsym.base_cases[8].interior_groups = [2, 2, 2, 2]
+BCTcube3dsym.base_cases[8].interior = [[(0, 4), (4, 5), 1, (1, 3)], [(1, 3), (3, 7), 5, (4, 5)], [(4, 5), (4, 6), 7, (3, 7)], [(3, 7), (2, 3), (4, 6), 6], [(0, 4), (4, 6), 2, (2, 3)], [(0, 4), (4, 5), (4, 6), (3, 7)], [(0, 4), (4, 6), (2, 3), (3, 7)], [(1, 3), (3, 7), (0, 4), (4, 5)], [(1, 3), (2, 3), (3, 7), (0, 4)], [(1, 3), (2, 3), 0, (0, 4)], [(2, 3), 2, 6, (4, 6)], [(3, 7), 7, (4, 6), 6], [(4, 5), 5, (3, 7), 7], [(4, 5), 5, (1, 3), 1], [(1, 3), 1, (0, 4), 0], [(2, 3), 2, (0, 4), 0]]
+BCTcube3dsym.base_cases[8].interior_groups = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 # 1,0,0,1,1,0,0,0 -> 00011001 # Basic Case 6
 BCTcube3dsym.base_cases[9].name = "MC33 Case 6.1.1"
 BCTcube3dsym.base_cases[9].faces = []
@@ -933,11 +933,11 @@ BCTcube3dsym.base_cases[3].tests = binaryheap((TEST_FACE_4, 0, CASE_IS_REGULAR))
 # 0,0,0,1,1,0,0,0 -> 00011000 # MC Case 4.2
 BCTcube3dsym.base_cases[8].mc33.append(Triangulation())
 BCTcube3dsym.base_cases[8].mc33[-1].name = "MC33 Case 4.2"
-BCTcube3dsym.base_cases[8].mc33[-1].faces = []
-BCTcube3dsym.base_cases[8].mc33[-1].exterior = []
-BCTcube3dsym.base_cases[8].mc33[-1].exterior_groups = [0, 0, 0, 0, 0, 0]
-BCTcube3dsym.base_cases[8].mc33[-1].interior = []
-BCTcube3dsym.base_cases[8].mc33[-1].interior_groups = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+BCTcube3dsym.base_cases[8].mc33[-1].faces = [[(1, 3), (3, 7), (4, 5)], [(4, 5), (4, 6), (3, 7)], [(4, 6), (2, 3), (3, 7)], [(1, 3), (4, 5), (0, 4)], [(2, 3), (1, 3), (0, 4)], [(2, 3), (0, 4), (4, 6)]]
+BCTcube3dsym.base_cases[8].mc33[-1].exterior = [[4, (0, 4), (4, 5), (4, 6)], [(0, 4), (4, 5), (4, 6), (3, 7)], [(0, 4), (2, 3), (4, 6), (3, 7)], [(0, 4), (1, 3), (3, 7), (4, 5)], [(0, 4), (1, 3), (2, 3), (3, 7)], [(1, 3), (2, 3), (3, 7), 3]]
+BCTcube3dsym.base_cases[8].mc33[-1].exterior_groups = [0,0,0,0,0,0]
+BCTcube3dsym.base_cases[8].mc33[-1].interior = [[(0, 4), (4, 5), 1, (1, 3)], [(1, 3), (3, 7), 5, (4, 5)], [(4, 5), (4, 6), 7, (3, 7)], [(3, 7), (2, 3), (4, 6), 6], [(0, 4), (4, 6), 2, (2, 3)], [(1, 3), (2, 3), (0, 4), 0], [(0, 4), 0, (1, 3), 1], [(0, 4), 0, (2, 3), 2], [(4, 5), 5, (1, 3), 1], [(3, 7), 7, (4, 5), 5], [(4, 6), 6, (3, 7), 7], [(4, 6), 6, (2, 3), 2]]
+BCTcube3dsym.base_cases[8].mc33[-1].interior_groups = [1,1,1,1,1,1,1,1,1,1,1,1]
 BCTcube3dsym.base_cases[8].tests = binaryheap((TEST_INTERIOR_3_4, 0, CASE_IS_REGULAR))
 # 1,0,0,1,1,0,0,0 -> 00011001 # MC33 Case 6.1.2
 BCTcube3dsym.base_cases[9].mc33.append(Triangulation())
