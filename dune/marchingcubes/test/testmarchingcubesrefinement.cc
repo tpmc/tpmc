87191fd5f37ae565d6eb2bed9419326efeb3cdd3
Oliver Sander <sander@dune-project.org>
2014-06-12 14:59:18 +0000

    Extend test to check whether integral over all subtriangles yields the complete volume
    [[Imported from SVN: r305]]

--- dune/marchingcubes/test/testmarchingcubesrefinement.cc
+++ dune/marchingcubes/test/testmarchingcubesrefinement.cc
@@ -6,9 +6,58 @@
 
 #include <iostream>
 
+#include <dune/geometry/quadraturerules.hh>
+
 #include <dune/marchingcubes/marchingcubesrefinement.hh>
 
 using namespace Dune;
+
+/* We integrate over both inside and outside and check whether the two volumes
+ * sum up to 1.0.
+ */
+template <int dim>
+void integralTest(GeometryType type,
+                  const std::vector<double> values)
+{
+  double volume = 0;
+
+  ///////////////////////////
+  // Sum over inside
+  ///////////////////////////
+  MarchingCubesRefinement<double,dim> interiorRefinement(type,values);
+
+  typename MarchingCubesRefinement<double,dim>::const_volume_iterator it = interiorRefinement.interiorBegin();
+
+  for (; it!=interiorRefinement.interiorEnd(); ++it)
+  {
+    // Get zero-order quadrature rule
+    const Dune::QuadratureRule<double,dim>& quad = Dune::QuadratureRules<double,dim>::rule(it->type(),0);
+
+    for (size_t i=0; i<quad.size(); i++)
+      volume += quad[i].weight() * it->integrationElement(quad[i].position());
+
+  }
+
+  ///////////////////////////
+  // Sum over outside
+  ///////////////////////////
+  MarchingCubesRefinement<double,2> exteriorRefinement(type,values,true);
+
+  for (it = exteriorRefinement.interiorBegin(); it!=exteriorRefinement.interiorEnd(); ++it)
+  {
+    // Get zero-order quadrature rule
+    const QuadratureRule<double,dim>& quad = Dune::QuadratureRules<double,dim>::rule(it->type(),0);
+
+    for (size_t i=0; i<quad.size(); i++)
+      volume += quad[i].weight() * it->integrationElement(quad[i].position());
+
+  }
+
+  if ( std::fabs(volume - ReferenceElements<double,dim>::general(type).volume()) > 1e-5 )
+  {
+    std::cout << "Integration over the entire refinement does not yield the correct element volume!" << std::endl;
+  }
+}
 
 // Very primitive testing of the Dune wrapper for the marching cubes algorithm
 int main(int argc, char* argv[]) try
@@ -19,6 +68,8 @@ int main(int argc, char* argv[]) try
   std::vector<double> values(4);
   values[0] = values[1] = -1;
   values[2] = values[3] =  1;
+
+  integralTest<2>(quadrilateral, values);
 
   MarchingCubesRefinement<double,2> refinement(quadrilateral,values);
 
