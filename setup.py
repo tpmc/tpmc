d73a26b2714067d8a7d76da89abe4c936819f206
Christian Engwer <christi@dune-project.org>
2015-04-10 14:03:59 +0200

    simplify installation of headers

--- setup.py
+++ setup.py
@@ -49,11 +49,7 @@ def configuration(parent_package='', top_path=None):
         config.header_dir = 'include/tpmc' # this is relative to config.path_in_package
         config.lib_dir = 'tpmc/lib' # this is relative to install path
         ## header files
-        # config.add_include_dirs(['tpmc/include/tpmc/*.hh', 'tpmc/lut/*.hh'])
-        config.add_data_files((config.header_dir, 'tpmc/include/tpmc/*.hh'))
-        config.add_data_files((config.header_dir, 'tpmc/lut/*.hh'))
-        #config.add_headers('tpmc/include/tpmc/*.hh'])
-        #config.add_headers('tpmc/lut/*.hh')
+        config.add_headers('tpmc/include/tpmc/*.hh')
         ## create build directory
         if not os.path.exists('build'):
                 os.mkdir('build')
