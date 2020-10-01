16f2cd1bb9b670df78fb9fc29ed4391e0893f8e8
Christian Engwer <christi@dune-project.org>
2020-10-01 17:10:49 +0200

    [cleanup] remove "always-true" if statement

--- tpmc/lut/lutgen/output.py
+++ tpmc/lut/lutgen/output.py
@@ -11,17 +11,16 @@ class Output:
         else:
             odir = ""
         for case in self.lg.base_cases:
-            if len(case.interior) > 0 or len(case.exterior) > 0 or 1==1:
-                self.write_case(case, case, self.lg.dim,
-                                ReferenceElements[self.lg.geometry_type],
+            self.write_case(case, case, self.lg.dim,
+			    ReferenceElements[self.lg.geometry_type],
 				odir+"%s%id-case%02i" % (self.lg.basic_type,
 							 self.lg.dim,i))
-                j = 1
-                if len(case.mc33) > 0:
-                    for mc33 in case.mc33:
-                        self.write_case(case, mc33, self.lg.dim,
-                                        ReferenceElements[self.lg.geometry_type],
-                                        odir+"%s%id-case%02i-%02i" % (self.lg.basic_type,
-                                                                      self.lg.dim,i,j))
-                        j += 1
-                i += 1
+	    j = 1
+	    if len(case.mc33) > 0:
+		for mc33 in case.mc33:
+		    self.write_case(case, mc33, self.lg.dim,
+				    ReferenceElements[self.lg.geometry_type],
+				    odir+"%s%id-case%02i-%02i" % (self.lg.basic_type,
+								  self.lg.dim,i,j))
+		    j += 1
+	    i += 1
