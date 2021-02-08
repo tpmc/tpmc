9099c6e5c60d3b04b6bad7bc8094b21b0bccd8f8
Christian Engwer <christi@dune-project.org>
2021-02-08 21:08:52 +0100

    untabify to fix python 3.7 error

--- tpmc/lut/lutgen/output.py
+++ tpmc/lut/lutgen/output.py
@@ -12,15 +12,15 @@ class Output:
             odir = ""
         for case in self.lg.base_cases:
             self.write_case(case, case, self.lg.dim,
-			    ReferenceElements[self.lg.geometry_type],
-				odir+"%s%id-case%02i" % (self.lg.basic_type,
-							 self.lg.dim,i))
-	    j = 1
-	    if len(case.mc33) > 0:
-		for mc33 in case.mc33:
-		    self.write_case(case, mc33, self.lg.dim,
-				    ReferenceElements[self.lg.geometry_type],
-				    odir+"%s%id-case%02i-%02i" % (self.lg.basic_type,
-								  self.lg.dim,i,j))
-		    j += 1
-	    i += 1
+                            ReferenceElements[self.lg.geometry_type],
+                                odir+"%s%id-case%02i" % (self.lg.basic_type,
+                                                         self.lg.dim,i))
+            j = 1
+            if len(case.mc33) > 0:
+                for mc33 in case.mc33:
+                    self.write_case(case, mc33, self.lg.dim,
+                                    ReferenceElements[self.lg.geometry_type],
+                                    odir+"%s%id-case%02i-%02i" % (self.lg.basic_type,
+                                                                  self.lg.dim,i,j))
+                    j += 1
+            i += 1
